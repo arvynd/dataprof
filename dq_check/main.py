3e96036749854965938eaab2fda6ed4bd985771c
Aravind <aravindpramod@outlook.com>
2025-08-17 00:02:31 +0530

    Nulls Check Functionality and Demo Dataset

--- dq_check/main.py
+++ dq_check/main.py
@@ -6,9 +6,21 @@ import click
 from rich.console import Console
 from rich.table import Table
 from loader import load_file
-from core import compute_basic_stats
+from core import compute_basic_stats, calculate_null_metrics
 
 console = Console()
+
+
+def df_to_rich_table(df, title=None):
+    """Convert a pandas DataFrame to a Rich Table."""
+    table = Table(show_header=True, header_style="bold magenta", title=title)
+    for col in df.columns:
+        table.add_column(str(col))
+
+    for _, row in df.iterrows():
+        table.add_row(*map(str, row.values))
+
+    return table
 
 
 @click.command()
@@ -19,19 +31,28 @@ def main(input, verbose):
     if verbose:
         console.log("[bold green]Verbose mode is ON")
 
+    # Load Data
     df = load_file(input)
+
+    # Get Basic Stats
     stats = compute_basic_stats(df)
+    stats_table = Table(title="Basic Dataset Statistics")
+    stats_table.add_column("Metric", style="cyan", no_wrap=True)
+    stats_table.add_column("Value", style="magenta")
 
-    table = Table(title="Basic Dataset Statistics")
+    stats_table.add_row("Row Count", str(stats["row_count"]))
+    stats_table.add_row("Column Count", str(stats["column_count"]))
+    stats_table.add_row("Column Names", ", ".join(stats["column_names"]))
 
-    table.add_column("Metric", style="cyan", no_wrap=True)
-    table.add_column("Value", style="magenta")
+    console.print(stats_table)
 
-    table.add_row("Row Count", str(stats["row_count"]))
-    table.add_row("Column Count", str(stats["column_count"]))
-    table.add_row("Column Names", ", ".join(stats["column_names"]))
+    # Get Null counts
+    console.log("Checking Nulls...")
+    nulls_df = calculate_null_metrics(df)
+    nulls_df = nulls_df.to_pandas()
 
-    console.print(table)
+    # Print nulls_df as a Rich Table
+    console.print(df_to_rich_table(nulls_df, title="Null Value Metrics"))
 
 
 if __name__ == "__main__":
